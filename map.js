c35d9202be0050637f6ca5af80751d1062cf84fe
Emmitt Stevenson <47986766+Hybrid232@users.noreply.github.com>
2025-09-29 15:41:18 -0600

    Pull Image
    Co-authored-by: matthewrowberry <matthewrowberry@users.noreply.github.com>
    Co-authored-by: Kronos5343 <Kronos5343@users.noreply.github.com>
    Co-authored-by: jacob-strong <jacob-strong@users.noreply.github.com>
    Co-authored-by: BrycePetrucka <BrycePetrucka@users.noreply.github.com>
    Co-authored-by: wahlquist <wahlquist@users.noreply.github.com>

--- map.js
+++ map.js
@@ -4,3 +4,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.marker([43.814672, -111.784795]).addTo(map)
+    .bindPopup('Here is this Location!<br> Look HERE!')
+    .openPopup();
+
+    
+var imageUrl = '/img/1.png',
+    imageBounds = [[43.814852, -111.785487], [43.814288, -111.784096]];
+L.imageOverlay(imageUrl, imageBounds, { opacity: 0.5 }).addTo(map);
+//43°48'51.3"N 111°47'02.5"W
+
