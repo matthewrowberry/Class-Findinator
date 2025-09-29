623bd38acd7db84a31f1d96d9660c42085f2ceff
Emmitt Stevenson <47986766+Hybrid232@users.noreply.github.com>
2025-09-29 15:01:10 -0600

    Push #3
    Co-authored-by: BrycePetrucka <BrycePetrucka@users.noreply.github.com>
    Co-authored-by: jacob-strong <jacob-strong@users.noreply.github.com>
    Co-authored-by: Kronos5343 <Kronos5343@users.noreply.github.com>
    Co-authored-by: matthewrowberry <matthewrowberry@users.noreply.github.com>

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([43.814654, -111.784797], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
