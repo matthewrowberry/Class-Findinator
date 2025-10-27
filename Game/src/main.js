5ce5e988649ef54706a36a69fde280d41f35d2dd
Kronos5343 <fuzzytaylor101@gmail.com>
2025-10-27 15:43:36 -0600

    in progress

--- Game/src/main.js
+++ Game/src/main.js
@@ -1,33 +1,22 @@
-import { Boot } from './scenes/Boot.js';
-import { Preloader } from './scenes/Preloader.js';
-import { Game } from './scenes/Game.js';
-import { GameOver } from './scenes/GameOver.js';
+import { Start } from './scenes/Start.js';
 
-//  Find out more information about the Game Config at:
-//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
+    title: 'Capture The Flaginator',
+    description: 'A Capture the flag game in the STC',
     parent: 'game-container',
-    backgroundColor: '#2d3436',
+    width: 2560,
+    height: 1440,
+    backgroundColor: '#FFFFFF',
+    pixelArt: false,
+    scene: [
+        Start
+    ],
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false,
-            gravity: { y: 0 }
-        }
-    },
-    scene: [
-        Boot,
-        Preloader,
-        Game,
-        GameOver
-    ]
-};
+}
 
-new Phaser.Game(config);
+const game = new Phaser.Game(config);
+            
---
+++ Game/src/scenes/Start.js
@@ -0,0 +1,60 @@
+export class Start extends Phaser.Scene {
+
+    constructor() {
+        super('Start');
+    }
+
+    preload() {
+        // The ship sprite is CC0 from https://ansimuz.itch.io - check out his other work!
+        this.load.image('background', 'assets/space.png');
+        this.load.spritesheet('ship', 'assets/spaceship.png', { frameWidth: 176, frameHeight: 96 });
+    }
+
+    create() {
+        this.background = this.add.tileSprite(1280, 720, 2560, 1440, 'background');
+
+
+        const ship = this.add.sprite(1280, 720, 'ship');
+
+        // world bounds
+        this.physics.world.setBounds(0, 0, 2560, 1440);
+
+        // background
+        this.add.tileSprite(0, 0, 2560, 1440, 'background').setOrigin(0, 0);
+
+        // player
+        this.player = this.physics.add.sprite(1280 / 2, 720 / 2, 'ship');
+
+        // camera
+        const cam = this.cameras.main;
+        cam.setBounds(0, 0, 2560, 1440);
+        cam.startFollow(this.player);
+        cam.setZoom(2);
+
+        // input
+        this.keys = this.input.keyboard.addKeys({
+        up: Phaser.Input.Keyboard.KeyCodes.W,
+        down: Phaser.Input.Keyboard.KeyCodes.S,
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D
+        });
+    }
+
+    update() {
+        const speed = 300;
+        this.ship.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.ship.setVelocityX(-speed);
+        } else if (this.cursors.right.isDown) {
+            this.ship.setVelocityX(speed);
+        }
+
+        if (this.cursors.up.isDown) {
+            this.ship.setVelocityY(-speed);
+        } else if (this.cursors.down.isDown) {
+            this.ship.setVelocityY(speed);
+        }
+    }
+    
+}
